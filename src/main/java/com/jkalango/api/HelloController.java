2ad4c386d1c9f3d7f0ee323b886db4120fcda109
smatheus-dev <you@example.com>
2025-06-18 11:37:17 -0300

    Primeiro Commit Spring

---
+++ src/main/java/com/jkalango/api/HelloController.java
@@ -0,0 +1,26 @@
+package com.jkalango.api;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/hello")
+
+public class HelloController {
+     
+    // HTTP- GET, POST, DELETE
+    // método java- foco HTTP-REST-RESTFULL
+    @GetMapping
+    public String mostrarMensagem(){
+        return "Hello  Spring!";
+    }
+
+}
+
+
+
+
+
+    
+
